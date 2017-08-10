aa08b3de93454a03d431829394e23fe25af67116
Edwuards <cesar.perez1327@gmail.com>
2017-08-09 18:07:26 -0700

    Advancement in Tools Object

--- JS/Main.js
+++ JS/Main.js
@@ -11,6 +11,15 @@ $(document).ready(function(e){
                         });
                     });
                 },
+                coordinate: {
+                    plot: function(data){
+                        var position = 'left:'+(x - 2)+'px; top:'+(y-2)+'px;';
+                        var attr = 'data-left="'+(x - 2)+'" data-top="'+(y-2)+'"';
+                        var div = '<div class="coordinate" '+attr+' style="'+position+'"></div>';
+
+                        $(data.artboard).append(div)
+                    }
+                },
                 settings: {
                     fillStyle: 'rgba(0,0,0,1)',
                     strokeStyle: 'rgba(0,0,0,1)',
@@ -20,17 +29,12 @@ $(document).ready(function(e){
                     lineJoin: 'round',
                     lineWidth: 1,
                     miterLimit: 10
-                },
-                /*
-                    where it was used
-                    what artboard id
-                    what layer id
-                    what index number
-                    what type string
-                    data collect
-                    data collection
-                */ 
+                }
             }
+            
+            /**************************************
+                        CONSTRUCTORS  
+            ***************************************/
             
             function Line(type){
                 this.type = type;
@@ -39,17 +43,63 @@ $(document).ready(function(e){
                 };
                 this.init = function(data){
                     $(data.artboard).on('click',function(e){
-                        this.exe(e);
+                        
+                        var x = ($(e.target).hasClass('coordinate') ? Number($(e.target).attr('data-x')) + 2 : e.offsetX);
+                  
+                        var y = ($(e.target).hasClass('coordinate') ? Number($(e.target).attr('data-y')) + 2 : e.offsetY);
+                        
+                        
+                        data.x = x;
+                        data.y = y;
+                        data.isFirst = true;
+                        data.event = e;
+                        
+                        if(this.position.x1 == null)
+                        {
+                            this.position.x1 = x;
+                            this.position.y1 = y;  
+                        }
+                        else
+                        {
+                            this.position.x2 = x;
+                            this.position.y2 = y;
+                            data.isFirst = false;
+                        }                        
+                        
+                        this.exe(data);
+                        
                     });
                 }
             }
+            Line.prototype = Object.create(Tool);
             Line.prototype.draw =  function(data){
-                var canvas  = data.canvas;
-                
+                var canvas  = data.layer;
+                canvas = canvas.getContext('2d');
+                for(var setting in this.settings)
+                {
+                    canvas[setting] = this.settings[setting];
+                }
+                canvas.beginPath();
+                canvas.moveTo(this.position.x1,this.position.y1);
+                canvas.lineTo(this.position.x2,this.position.y2);
+                canvas.closePath();
+                canvas.stroke();
+            }
+                    
+            
+            /**************************************
+                          INSTANCES  
+            ***************************************/
+            
+            
+            var line = new Line('line');
+            line.exe = function(data){
+                if(data.isFirst)
+                {   
+                    this.coordinate.plot(data.x,data.y);
+                }
             }
             
-        
-            var line = new Line('line');
             
         }
         
